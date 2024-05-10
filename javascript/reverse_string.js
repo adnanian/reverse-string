ddc9bc47fc78dab2db2d87dbfafb3ed43d3be13c
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-05-10 09:48:16 -0500

    Complete challenge

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str.charAt(i);
+  }
+  return reversedStr;
 }
 
 if (require.main === module) {
@@ -17,3 +22,21 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/**
+ * 1. Reverse the sequence of characters. 2. Understood
+ * 
+ * 3. abcdefg => gfedcba
+ *    Adnan => nandA
+ *    Hi, I'm John => nhoJ m'I ,iH
+ * 
+ * 4. Puesdocode
+ * 
+ * function reverse(string) {
+ *  let reversedString = ""
+ *  for (let i = string.length - 1; i >= 0; i--) {
+ *    reveresedString += string[i]
+ *  }
+ *  return reversedString
+ * } 
+ */
--- javascript/tests/reverse_string.test.js
+++ javascript/tests/reverse_string.test.js
@@ -1,5 +1,5 @@
 const reverseString = require('../reverse_string');
-
+/*
 test("can handle an empty string", () => {
   expect(reverseString("")).toBe("");
 });
@@ -19,3 +19,18 @@ test("can handle three characters", () => {
 test("can handle many characters", () => {
   expect(reverseString("sham-meow")).toBe("sham-meow".split("").reverse().join(""));
 });
+*/
+
+// Adnan Tests
+
+test("Adnan Test #1", () => {
+  expect(reverseString("abcdefg")).toBe("gfedcba");
+})
+
+test("Adnan Test #2", () => {
+  expect(reverseString("Adnan")).toBe("nandA");
+})
+
+test("Adnan Test #3", () => {
+  expect(reverseString("Hi, I\'m John")).toBe("Hi, I\'m John".split("").reverse().join(""));
+})
